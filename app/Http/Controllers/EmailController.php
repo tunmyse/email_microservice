e6c00c47b852f04c509531a84705238e9407e497
Oluwatunmise Akinsola <akinsolatunmise@gmail.com>
2019-08-12 00:37:41 +0100

    Add api routes and controller methods

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function send(Request $request)
+    {
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/sendemail', 'EmailController@send');
+Route::get('/email', 'EmailController@index');
