071d875c3535329770dbb05f658afbf09243bd50
Oluwatunmise Akinsola <akinsolatunmise@gmail.com>
2019-08-19 11:46:54 +0100

    Create email list component

--- resources/js/components/EmailListComponent.vue
+++ resources/js/components/EmailListComponent.vue
@@ -1,8 +1,59 @@
 <template>
-  
+  <div class="container">
+    <div class="row">
+      <table class="table table-striped table-hover">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Subject</th>
+            <th scope="col">Body</th>
+            <th scope="col">Format</th>
+            <th scope="col">View Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(email, index) in emails">
+            <th scope="row">{{ index + 1}}</th>
+            <td>{{ email.subject }}</td>
+            <td class="text-truncate">{{ email.body }}</td>
+            <td>{{ email.format }}</td>
+            <td>
+              <button class="btn btn-secondary" v-on:click="viewStatus(index)">View Status</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  
+  mounted() {
+    console.log("List component mounted.");
+    axios.get('/api/email').then((res)=>{
+          this.emails = res.data.data;
+      });
+  },
+
+  data() {
+    return {
+      emails: [],
+      error: null,
+      loading: false
+    };
+  },
+
+  methods: {
+    viewStatus: function(index) {
+        console.log(this.emails)
+      this.$router.push({
+        name: "status",
+        params: {
+          email: this.emails[index]
+        }
+      });
+    }
+  }
+};
 </script>
